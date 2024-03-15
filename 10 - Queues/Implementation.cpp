3339e5714be755ec3ca96c4e3432f5be9587577f
sanyam <sanyamnarang40@gmail.com>
2024-03-16 01:29:14 +0530

    queue implementation added

--- 10 - Queues/Implementation.cpp
+++ 10 - Queues/Implementation.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // push -> rear se hoga
 // pop -> front se hoga
 
+// TC : 0(1) in all cases as middle elements aren't accessed
+
 class Queue{
 
     public:
@@ -37,9 +39,87 @@ class Queue{
         }
     }
 
+    void pop(){
+        if(front==-1 && rear==-1){
+            cout << "Queue Underflow" << endl;
+            return;
+        }
+        else if(front==rear){
+            // single element case
+            arr[front]=-1;
+            // reset front & rear
+            front =-1;
+            rear=-1;
+        }
+        else{
+            arr[front]=-1;
+            front++;
+        }
+    }
 
+    bool isEmpty(){
+        if(front==-1 && rear==-1) return true;
+        else return false;
+    }
+
+    int getSize(){
+        if(front==-1 && rear==-1) return 0;
+        else return rear-front+1;
+    }
+
+    int getFront(){
+        if(front==-1){
+            cout << "No Element in queue" ;
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+
+    int getRear(){
+        if(rear==-1){
+            cout << "No element is queue";
+            return -1;
+        }
+        else return arr[rear];
+    }
+
+    void print(){
+        for(int i=0;i<size;i++){
+            cout << arr[i]<<" ";
+        }
+        cout << endl;
+    }
 };
 
 int main(){
 
+    Queue q(5);
+    q.print();
+
+    q.push(10);
+    q.print();
+
+    q.push(20);
+    q.print();
+
+    q.push(30);
+    q.print();
+
+    q.push(40);
+    q.print();
+
+    q.push(50);
+    q.print();
+
+    cout << q.getSize()<<endl;
+
+    q.pop();
+    q.print();
+    cout << q.getSize()<<endl;
+
+    q.push(100); // overflow 
+
+    cout << q.getFront() << endl;
 }
